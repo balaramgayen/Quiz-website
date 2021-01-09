18e334698e535429420a396ab1946f69999c0bec
balaram gayen <balaramgayen53501@gmail.com>
2021-01-09 19:20:46 +0530

    create footer

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import "./footer.css";
 
 class Footer extends Component {
     state = {  }
     render() { 
         return ( 
             <React.Fragment>
-                    <div class="footer-copyright mt-5 mb-0 text-center py-3 bg-dark text-white">
+                    <div id="footer" class="footer-copyright text-cente">
                         2020 © Copyright :
                         all rights reserved<br></br>
                         Design & Developed By <a href="https://balaramgayen.blogspot.com/" target="blank">Balaram Gayen</a><br></br>
